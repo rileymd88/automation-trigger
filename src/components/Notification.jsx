f54d0735a4dc533000f67e75ec06491b00ed8898
rileymd88 <riley.macdonald@gmail.com>
2021-02-17 14:10:18 +0100

    fixed notification hidden issue

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/core/Alert';
+import Portal from '@material-ui/core/Portal';
+import { makeStyles } from '@material-ui/core/styles';
 import { selectMessage, selectSnackbarOpen, selectSeverity, setSnackbarOpen } from '../states/formsSlice'
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -9,8 +11,15 @@ const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
+const useStyles = makeStyles((theme) => ({
+  notification: {
+    zIndex: 2000
+  },
+}));
+
 export default function Notification() {
   const dispatch = useDispatch()
+  const classes = useStyles()
   const showSnackbar = useSelector(selectSnackbarOpen)
   const message = useSelector(selectMessage)
   const severity = useSelector(selectSeverity)
@@ -21,14 +30,18 @@ export default function Notification() {
   }
 
   return (
+    <Portal>
     <Snackbar
       anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
       open={showSnackbar} 
-      autoHideDuration={6000} 
+      autoHideDuration={12000} 
       onClose={handleClose}>
-      <Alert onClose={handleClose} severity={severity}>
+      <Alert 
+      onClose={handleClose} 
+      severity={severity}>
         {message}
       </Alert>
     </Snackbar>
+    </Portal>
   );
 }
